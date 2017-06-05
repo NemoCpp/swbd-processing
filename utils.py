2f97e36352188b9bf21ec1435b554660d5ce83f4
Allan Jiang <origami789@gmail.com>
2017-06-05 10:24:19 -0700

    segment speaker times

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import os
+import re
+from collections import defaultdict
 
 def extension(fname, ext):
     return fname.lower().endswith(ext)
@@ -12,19 +14,58 @@ def dir2id(path, ext):
             out.append(os.path.splitext(fpath)[0])
     return out
 
-## need last 4 chars of filename without extension to get id
-audio_files = map(lambda x: x[-4:], dir2id('cd01/swb1', '.sph'))
-trans_files = map(lambda x: x[-4:], dir2id('phase1/disc01', '.mrk'))
-
-common = set.intersection(set(audio_files), set(trans_files))
-print sorted(common)
-
-def poop(audio_files, trans_files):
+def raw_speaker_times(audio_path, trans_path):
     """
-    Start with map, indexed on speaker ID.
-    For each audio file
-        grab the speaker IDs
-        look at mrt and txt and append tuples of (audio_fileid, range-to-snip) to each speaker ID.
-    Later, we will poop these out.
+    outputs a map
+    key: speaker id
+    value: tuple of (file id, start time, end time)
+    values describe speaker durations in large contiguous blocks
     """
+    out = defaultdict(list)
 
+    ## need last 4 chars of filename without extension to get id
+    audio_files = map(lambda x: x[-4:], dir2id(audio_path, '.sph'))
+    trans_files = map(lambda x: x[-4:], dir2id(trans_path, '.mrk'))
+
+    common = set.intersection(set(audio_files), set(trans_files))
+
+    convos = {}
+    for fid in list(common):
+        with open(os.path.join(trans_path, 'sw'+fid+'.txt'), 'r') as txtf:
+            firstline = txtf.readline().strip()
+            _,A,B = firstline.split('\t')[1].split('_')
+        convos[fid] = {'A': A, 'B': B}
+        # print fid,A,B
+
+    for fid in list(common):
+        with open(os.path.join(trans_path, 'sw'+fid+'.mrk'), 'r') as mrtf:
+            data = mrtf.readlines()
+            data = map(lambda x: re.sub('[ \t]+',' ',x.strip()).split(' '), data)
+        prev = None
+        startt = 0
+        for info in data:
+            sid,start,duration,_ = info
+            if prev==sid:
+                continue
+            else:
+                prev=sid
+                if sid in convos[fid]:
+                    out[convos[fid][sid]].append((fid,startt,start))
+                startt = start
+    return out
+
+raw = raw_speaker_times('cd01/swb1', 'phase1/disc01')
+
+# for sid, desc in out.iteritems():
+#     fid,start,end = desc
+def chunk_times(durations):
+    """
+    input: list of tuples of (file id, start time, end time)
+    output: new list tuples that breaks speaker durations into short snippets, between 2 and 8 seconds long
+    """
+    out = []
+    for fid,start,end in durations:
+        out.append(fid)
+    print out
+
+chunk_times(raw['1122'])
