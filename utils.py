ea55436b275ff9ea82291d8377acee174add18e8
Chaitanya Asawa <casawa@stanford.edu>
2017-06-05 21:27:45 -0700

    Finish util.py

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import os
 import re
 from collections import defaultdict
+from python_speech_features import mfcc
+import scipy.io.wavfile as wav
+
+WIN_LEN = 0.125
 
 def extension(fname, ext):
     return fname.lower().endswith(ext)
@@ -80,13 +84,23 @@ def chunk_times(durations, chop_time=4):
                 out.append((fid,s+i*chop_time,s+(i+1)*chop_time))
     return out
 
-def desc2nppath(desc):
+def desc2nppath(segment_num, desc):
     """
     Given a description, of (file id, start time, end time), where times are in seconds,
     compute the corresponding np array for that audio snippet, write it to a file, and
     return the corresponding path string of that np snippet file.
     """
-    return str(desc)
+    file_id, start, end = desc
+    speech_file_name = 'cd01/swb1/sw0' + file_id + '.wav'
+    print speech_file_name
+    rate, sig = wav.read(speech_file_name)
+    mfcc_feat = np.array(mfcc(sig, rate, winlen=WIN_LEN, winstep=WIN_LEN))
+    start_index = int(start / WIN_LEN)
+    end_index = int(end / WIN_LEN)
+
+    output_file = 'features/' + file_id + '_' + str(segment_num) + '.npy'
+    mfcc_feat[start_index : end_index].dump(output_file)
+    return output_file
 
 def poop(raw):
     out={}
@@ -96,8 +110,8 @@ def poop(raw):
 
     entries = []
     for sid, descs in out.iteritems():
-        for desc in descs:
-            entry = '\t'.join([str(sid),desc2nppath(desc)])
+        for i, desc in enumerate(descs):
+            entry = '\t'.join([str(sid),desc2nppath(i, desc)])
             entries.append(entry)
     with open('catalog', 'w') as text_file:
         text_file.write('\n'.join(entries))
